95dc85708fea609245fbb1a35574aa0b2c9a8f01
Anwesh43 <anweshthecool0@gmail.com>
2018-07-21 11:51:53 +0530

    adding ThreeUpBallView class

--- threeupballview/src/main/java/com/anwesh/uiprojects/threeupballview/ThreeUpBallView.kt
+++ threeupballview/src/main/java/com/anwesh/uiprojects/threeupballview/ThreeUpBallView.kt
@@ -12,3 +12,20 @@ import android.graphics.Canvas
 
 val NODES : Int = 5
 
+class ThreeUpBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
