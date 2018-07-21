510b56fee79485ccf5efbc3272f5526adad507e0
Anwesh43 <anweshthecool0@gmail.com>
2018-07-21 12:04:46 +0530

    drawing canvas extension methods

--- threeupballview/src/main/java/com/anwesh/uiprojects/threeupballview/ThreeUpBallView.kt
+++ threeupballview/src/main/java/com/anwesh/uiprojects/threeupballview/ThreeUpBallView.kt
@@ -12,6 +12,25 @@ import android.graphics.Canvas
 
 val NODES : Int = 5
 
+fun Canvas.drawTUBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / NODES
+    val sc1 : Float = Math.min(scale, 0.5f) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0.0f, scale - 0.5f)) * 2
+    var x : Float = gap
+    if (i == NODES - 1) {
+        x = gap * 0.4f
+    }
+    save()
+    translate(gap/2 + i * x * sc2, h/2)
+    for (j in 0..2) {
+        val yGap : Float = gap * (1 - j) * (1 - sc1)
+        drawCircle(0f, yGap, 0.1f * gap, paint)
+    }
+    restore()
+}
+
 class ThreeUpBallView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
